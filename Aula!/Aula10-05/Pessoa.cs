97618f306ae6d470b5fd4bc33a3abddf0c363c95
carlos alexandre Neppel <carlos_neppel@hotmail.com>
2018-05-10 10:31:41 -0300

    adicionando classe pessoa

---
+++ Aula!/Aula10-05/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula10_05
+{
+    class Pessoa
+    {
+    }
+}
