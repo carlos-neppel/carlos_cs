0dfc6d6f40c5c286c15bc485abda80b39e72d925
carlos alexandre Neppel <carlos_neppel@hotmail.com>
2018-05-10 08:52:02 -0300

    aula de  segunda

---
+++ Aula!/Aula07-05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula07_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+
+            Console.WriteLine("Hello Words");
+            Console.WriteLine("a:" + a);
+           
+            if (Console.CapsLock)
+                Console.WriteLine("CapsLock ativado");
+            else
+                Console.WriteLine("CapsLock desativado");
+
+            Console.ReadKey();
+           
+
+
+        }
+    }
+}
