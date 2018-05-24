1eeba5589415dc32e735644ac16dfe03507216d6
carlos alexandre Neppel <carlos_neppel@hotmail.com>
2018-05-24 11:20:29 -0300

    tentando programar

--- ControleAtividades/Controllers/AtividadesController.cs
+++ ControleAtividades/Controllers/AtividadesController.cs
@@ -1,12 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using Modelos;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Controllers
 {
     class AtividadesController
     {
+        private static List<Atividade> ListaAtividade { get; set; }
+
+        private static int uUtimoIdUtilizado = 1;
+        static AtividadesController()
+        {
+            ListaAtividade = new List<Atividade>();
+        }
+
+        //salvar
+        public void Salvar(Atividade atividade)
+        {
+            atividade.AtividadeID = uUtimoIdUtilizado++;
+            ListaAtividade.Add(atividade);
+        }
+        //Listar
+
+        public List<Atividade> Listar()
+        {
+            return ListaAtividade;
+        }
+
+        //BsucarPorId
+
+        public Atividade BuscarPorId(int id)
+        { 
+            foreach (Atividade a in ListaAtividade)
+            {
+                if (a.AtividadeID == id)
+                {
+                    return a;
+                }
+            }
+
+            return null;
+
+        }
+
+        //BuscarPorNome
+
+        public List<Atividade>  BuscarPorNome(string nome)
+        {
+            IEnumerable<Atividade> AtividadeSelecionada = new List<Atividade>();
+            AtividadeSelecionada = from x in ListaAtividade
+                                   where x.
+
+        }
+            
+
+
+
     }
 }
