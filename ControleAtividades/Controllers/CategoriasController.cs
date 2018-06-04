aef911ca57e21b4e2f791808f21d8c4eeec2c849
carlos alexandre Neppel <carlos_neppel@hotmail.com>
2018-06-04 11:02:39 -0300

    ultima etapa  da aula

--- ControleAtividades/Controllers/CategoriasController.cs
+++ ControleAtividades/Controllers/CategoriasController.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Controllers.Base;
+using Modelos;
 
 namespace Controllers
 {
-    public class CategoriasController
+    public class CategoriasController : IBaseController<Categoria>
     {
+        public void Adicionar(Categoria entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(Categoria entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Categoria BuscarPorID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Categoria> ListarPorNome(string nome)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Categoria> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ControleAtividades/Controllers/UsuariosController.cs
+++ ControleAtividades/Controllers/UsuariosController.cs
@@ -3,11 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Controllers.Base;
+using Modelos;
 
 namespace Controllers
 {
-    class UsuariosController
+    class UsuariosController : IBaseController<Usuario>
     {
+        private Contexto contexto = new Contexto();
+
+        {
+            
+        public void Adicionar(Usuario entity)
+            {
+               
+            }
+
+            public void Atualizar(Usuario entity)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Usuario BuscarPorID(int id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Excluir(int id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IList<Usuario> ListarPorNome(string nome)
+            {
+               return contexto.Usuarios.Where(uint => u)
+            }
+
+            public IList<Usuario> ListarTodos()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
 
     }
 }
